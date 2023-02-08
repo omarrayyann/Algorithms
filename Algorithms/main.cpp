125064e9d0cd48cec78ba11cad8fc7e81a22b0e4
Omar <olr7742@nyu.edu>
2023-02-08 21:02:15 +0400

    Initial Commit

---
+++ Algorithms/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Algorithms
+//
+//  Created by Omar Rayyan on 08/02/2023.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
