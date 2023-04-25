de8d12652b43f001cde6f0a799e7e2fb504c9188
Omar <olr7742@nyu.edu>
2023-04-25 21:13:17 +0400

    Bellman Ford Update

--- bellman_ford.cpp
+++ bellman_ford.cpp
@@ -1,24 +1,24 @@
-#include &ltiostream &gt
-#include &ltvector &gt
-#include &ltclimits &gt
+#include <iostream>
+#include <vector>
+#include <climits>
 
 using namespace std;
 
 // {From, To}, Cost
-typedef pair &ltvector &ltint &gt, int &gt edge;
+typedef pair<vector<int>, int> edge;
 
-vector &ltint &gt bellman_ford(vector &ltvector &ltint &gt &gt &graph, int starting)
+vector<int> bellman_ford(vector<vector<int>> &graph, int starting)
 {
 
-    vector &ltint &gt distances;
+    vector<int> distances;
     distances.resize(graph.size(), INT_MAX);
     distances.at(starting) = 0;
 
-    vector &ltvector &ltint &gt &gt edges;
+    vector<vector<int>> edges;
 
-    for (int i = 0; i & lt graph.size(); i++)
+    for (int i = 0; i < graph.size(); i++)
     {
-        for (int j = 0; j & lt graph.at(0).size(); j++)
+        for (int j = 0; j < graph.at(0).size(); j++)
         {
             if (graph.at(i).at(j) != 0)
             {
@@ -28,7 +28,7 @@ vector &ltint &gt bellman_ford(vector &ltvector &ltint &gt &gt &graph, int start
     }
 
     // Relaxing the edges until no changes occur in an iteration (m-1) in the worst case
-    for (int i = 0; i & lt graph.size(); i++)
+    for (int i = 0; i < graph.size(); i++)
     {
         bool change = false;
         for (auto edge : edges)
@@ -36,7 +36,7 @@ vector &ltint &gt bellman_ford(vector &ltvector &ltint &gt &gt &graph, int start
             int from = edge[0];
             int to = edge[1];
             int weight = graph.at(from).at(to);
-            if (distances[from] != INT_MAX && distances[from] + weight & lt distances[to])
+            if (distances[from] != INT_MAX && distances[from] + weight < distances[to])
             {
                 distances[to] = distances[from] + weight;
                 change = true;
@@ -54,13 +54,13 @@ vector &ltint &gt bellman_ford(vector &ltvector &ltint &gt &gt &graph, int start
 int main()
 {
 
-    vector &ltvector &ltint &gt &gt graph = {
+    vector<vector<int>> graph = {
         {0, 1, 0, 2, 0}, {0, 0, 2, 0, 0}, {0, 0, 0, 0, 8}, {0, 0, 0, 0, 3}, {0, 0, 0, 0, 0}};
 
-    vector &ltint &gt ans = bellman_ford(graph, 0);
+    vector<int> ans = bellman_ford(graph, 0);
 
-    for (int i = 0; i & lt ans.size(); i++)
+    for (int i = 0; i < ans.size(); i++)
     {
-        cout &lt &lt i &lt &lt ": " & lt &lt ans.at(i) & lt &lt endl;
+        cout << i << ": " << ans.at(i) << endl;
     }
 }
