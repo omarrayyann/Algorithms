a5c3160de896b2da456bd0b1987ae62285788055
Omar <olr7742@nyu.edu>
2023-05-05 17:36:52 +0400

    Update

---
+++ Assignment 4 - Q2.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+
+int **mem;
+
+int maxWeightIndependentSet(int i, int)
+
+    int main()
+{
+
+    mem = new int *[n + 1];
+    for (int i = 0; i < n + 1; i++)
+    {
+        mem[i] = new int[n + 1];
+        for (int j = 0; j < n + 1; j++)
+        {
+            mem[i][j] = -1;
+        }
+    }
+    cout << p(n, n - 1) << endl;
+}
